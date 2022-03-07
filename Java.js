84336a2308cb58a5610fa77b87699f8dac3cc4bd
LorenzoPassalacqua <lorenzopassalacqua95@gmail.com>
2022-03-07 14:16:25 +0000

    javascript test added

---
+++ Java.js
@@ -0,0 +1,12 @@
+function My_First_Function() {
+    var str="this text is green";
+    var result= str.fontcolor("green");
+    document.getElementById("Green_text").innerHTML=result; //this is the script for the button
+}
+
+window.alert("this is an alert!") //alert window
+
+document.write("hello world!" + "Today is a really nice day!" + "We should go for a walk") //paragraph before the button
+
+
+
