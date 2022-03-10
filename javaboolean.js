3847553520cc98675cb3315d6d9c3c79eeb3ca2e
LorenzoPassalacqua <lorenzopassalacqua95@gmail.com>
2022-03-10 14:50:33 +0000

    added or || and && to the script

--- javaboolean.js
+++ javaboolean.js
@@ -3,14 +3,22 @@ function boo(){
     var y= document.getElementById("n2").value;
     
     if (x > y){
-       return document.getElementById("b").innerHTML= document.write("true");
+      document.getElementById("b").innerHTML= document.write("true, ");
     }
 
-    if (!(x > y)){
-       return document.getElementById("b").innerHTML= document.write("false");
+    if (!(x >= y)){
+      document.getElementById("b").innerHTML= document.write("false, ");
     }
 
     if (x == y){
-      return  document.getElementById("b").innerHTML= document.write("equal");
+      document.getElementById("b").innerHTML= document.write("equal, ");
+    }
+
+    if (x > 5 && y > 2){
+      document.getElementById("b").innerHTML= document.write("x is greater than 5 and y is greater than 2 ")
+    }
+
+    if (x > 10 || y > 10){
+       document.getElementById("b").innerHTML= document.write("one of the numbers is greater is than 10 ")
     }
 }
