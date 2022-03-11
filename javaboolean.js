cc8c23a0e00049c30f6fbdf3c31371d39af3072a
LorenzoPassalacqua <lorenzopassalacqua95@gmail.com>
2022-03-11 15:23:13 +0000

    fixed everything in both html and javascript

--- javaboolean.js
+++ javaboolean.js
@@ -1,24 +1,36 @@
 function boo(){
-    var x= document.getElementById("n1").value;
-    var y= document.getElementById("n2").value;
-    
+    var b= document.getElementById("b");
+    var b1= document.getElementById("b1");
+    var b2= document.getElementById("b2");
+    var b3= document.getElementById("b3");
+    var b4= document.getElementById("b4");
+
+    b.innerHTML=""
+    b1.innerHTML=""
+    b2.innerHTML=""
+    b3.innerHTML=""
+    b4.innerHTML=""
+
+    var x= parseInt(document.getElementById("n1").value);
+    var y= parseInt(document.getElementById("n2").value);
+
     if (x > y){
-      document.getElementById("b").innerHTML= document.write("true, ");
+      b.innerHTML="true ";
     }
 
     if (!(x >= y)){
-      document.getElementById("b").innerHTML= document.write("false, ");
+      b1.innerHTML="false ";
     }
 
     if (x == y){
-      document.getElementById("b").innerHTML= document.write("equal, ");
+      b2.innerHTML="equal ";
     }
 
     if (x > 5 && y > 2){
-      document.getElementById("b").innerHTML= document.write("x is greater than 5 and y is greater than 2 ")
+      b3.innerHTML="x is greater than 5 and y is greater than 2 ";
     }
 
     if (x > 10 || y > 10){
-       document.getElementById("b").innerHTML= document.write("one of the numbers is greater is than 10 ")
+       b4.innerHTML="one of the numbers is greater is than 10 ";
     }
 }
