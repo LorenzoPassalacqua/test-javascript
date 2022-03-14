c9c0c342d1fa0e9d735e5af4a97af778c7b1fb58
LorenzoPassalacqua <lorenzopassalacqua95@gmail.com>
2022-03-14 15:02:34 +0000

    added the getdate and gethours

--- javaternary.js
+++ javaternary.js
@@ -1,5 +1,5 @@
 function maggiorenne(){
-    var x= document.getElementById("adult").value;
+    var x=  document.getElementById("adult").value;
     function mag(){    
     var maggiorenne= (x < 18) ? "you are too young to vote" : "you are old enought to vote";
     return maggiorenne;
@@ -18,4 +18,21 @@ function voter(){
   const mvoter = new nvoter(document.getElementById("name").value, document.getElementById("surname").value);
   document.getElementById("full").innerHTML =
   mvoter.name + " " + mvoter.surname;
+}
+
+
+
+
+function timer(){
+  const d= new Date()
+  const t= new Date()
+  var y=  document.getElementById("adult").value;
+
+
+  if (parseInt(y < 18)){
+    document.getElementById("time").innerHTML= d.getDate();
+  }
+  else {
+      document.getElementById("time").innerHTML= t.getHours();
+    }
 }
